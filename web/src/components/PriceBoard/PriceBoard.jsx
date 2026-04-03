eac82220b08ee844323cdbeb2e0f2a56415ef632
nhd1311 <ducnguyenhuu1311@gmail.com>
2026-04-03 16:54:05 +0700

    create stock table & edit css

--- web/src/components/PriceBoard/PriceBoard.jsx
+++ web/src/components/PriceBoard/PriceBoard.jsx
@@ -1,10 +1,12 @@
 import IndexTable from '../IndexTable/IndexTable';
+import StockBoard from '../StockBoard/StockBoard';
 import './PriceBoard.scss';
 
 function PriceBoard() {
   return (
     <div className="price-board">
       <IndexTable />
+      <StockBoard />
     </div>
   );
 }
